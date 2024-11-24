b5b12478ff4243c2a364523c8358da79ed3833a9
Jabir Hussain <c.jabir123@gmail.com>
2024-11-24 22:47:04 +0000

    MAIN MENU

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+<<<<<<< Updated upstream
     public void PlayGame()
+=======
+    public void StartGame()
+>>>>>>> Stashed changes
     {
         SceneManager.LoadSceneAsync(0);
     }
 
+<<<<<<< Updated upstream
     // Update is called once per frame
+=======
+>>>>>>> Stashed changes
     public void QuitGame()
     {
         Application.Quit();
